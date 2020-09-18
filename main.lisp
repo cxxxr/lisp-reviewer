7e681d65d4c0ae840cf99f231a1455d17f8e7e00
cxxxr <g23tlm@gmail.com>
2020-09-19 02:06:52 +0900

    move function

--- main.lisp
+++ main.lisp
@@ -20,21 +20,6 @@
                          defpackage-reviewer)
   ())
 
-(defun review-file (reviewer pathname)
-  (let* ((buffer (lem-base:find-file-buffer pathname
-                                            :temporary t
-                                            :enable-undo-p nil
-                                            :syntax-table lem-lisp-syntax:*syntax-table*))
-         (point (lem-base:buffer-point buffer)))
-    (review reviewer point)
-    (when (lem-base:buffer-modified-p buffer)
-      (lem-base:write-to-file buffer (lem-base:buffer-filename buffer)))
-    buffer))
-
-(defun review-system (reviewer system-name)
-  (dolist (pathname (sblint/utilities/asdf:all-project-pathnames-for-package-inferred-system system-name))
-    (review-file reviewer pathname)))
-
 (defun test ()
   (handler-bind ((comment (lambda (c)
                             (format t "~&~A~&" c)
--- reviewer.lisp
+++ reviewer.lisp
@@ -1,7 +1,9 @@
 (defpackage :lisp-reviewer/reviewer
   (:use :cl)
   (:export :review
-           :reviewer))
+           :reviewer
+           :review-file
+           :review-system))
 (in-package :lisp-reviewer/reviewer)
 
 (defgeneric review (reviewer point)
@@ -9,3 +11,18 @@
 
 (defclass reviewer ()
   ())
+
+(defun review-file (reviewer pathname)
+  (let* ((buffer (lem-base:find-file-buffer pathname
+                                            :temporary t
+                                            :enable-undo-p nil
+                                            :syntax-table lem-lisp-syntax:*syntax-table*))
+         (point (lem-base:buffer-point buffer)))
+    (review reviewer point)
+    (when (lem-base:buffer-modified-p buffer)
+      (lem-base:write-to-file buffer (lem-base:buffer-filename buffer)))
+    buffer))
+
+(defun review-system (reviewer system-name)
+  (dolist (pathname (sblint/utilities/asdf:all-project-pathnames-for-package-inferred-system system-name))
+    (review-file reviewer pathname)))
